1da07fb463b3f9374e656d6f63e65b56033f49f0
AlicjaBobko <alicja_bobko@o2.pl>
2022-05-17 11:11:15 +0200

    elements generated from data.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,31 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Travel from './components/Travel';
+import data from './data';
 
 export default function App() {
+  const travels = data.map((item, index, data) => {
+    let isLast = (data.length - 1 === index) ? true : false;
+    return (
+      <div>
+        <Travel
+          key={item.id}
+          {...item}
+          line={isLast}
+        />
+      </div>
+    )
+  })
   return (
     <div className='wrapper'>
       <Navbar />
       <div className='travel--list'>
-        <Travel />
-      <Travel />
-        <Travel />
+        
+        {travels}
         </div>
      </div>
   )
-  }
+}
+
   
 
--- src/components/Travel.js
+++ src/components/Travel.js
@@ -1,23 +1,27 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 
-export default function Travel() {
+export default function Travel(props) {
+    const { title, location, googleMapsUrl, startDate, endDate, description, imageURL, line } = props;
     return (
-    
+        <div>
             <div className="travel">
-            < img src="https://source.unsplash.com/WLxQvbMyfas" alt="fiji" className='travel--image' />
+            < img src={imageURL} alt={title} className='travel--image' />
             <div className="travel--text">
             <span>
-            <span className="travel--location"><FontAwesomeIcon icon={faLocationDot} className="travel--pin" />Japan</span>
-                <a href="https://www.google.com/maps/d/u/0/embed?mid=1W9AtcwnYAqP1yVHr5YjhwmQa0eM&ie=UTF8&hl=en&msa=0&ll=35.36525600000006%2C138.737411&spn=0.09799%2C0.145912&z=12&output=embed" className="google--maps">View on Google Maps</a>
+            <span className="travel--location"><FontAwesomeIcon icon={faLocationDot} className="travel--pin" />{location}</span>
+                <a href={googleMapsUrl} className="google--maps" target="_blank">View on Google Maps</a>
                 </span>
-                <h2>Mount Fuji</h2>
-                <span className="travel--date">12 Jan, 2021- 24 Jan, 2021</span>
+                <h2>{title}</h2>
+                <span className="travel--date">{startDate}, {endDate }</span>
                 <section className="travel--desc">
-                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+                    {description}
                 </section>
-                </div>
+            </div>
             </div> 
+            {!line && <hr className="line"></hr>}
+            </div>
     )
 }
